8ee47b8c285016e1952922531d3ecc17a51afb84
James Gifford <jgifford@discogsinc.com>
2019-04-30 16:53:03 -0700

    Copy previous site files

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,91 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+    <title>Jacob Peak Photography</title>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:700|Roboto" rel="stylesheet">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/lightbox.css">
+    <link rel="stylesheet" href="/css/styles.css">
+</head>
+<body>
 
-            .full-height {
-                height: 100vh;
-            }
+    <div class="container">
+        <div class="row">
+            <h1>Jacob Peak Photography</h1>
+        </div>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+        <div class="row header">
+            <div class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3">
+                <img src="/photos/jacob_homepage.png" class="img-responsive" alt="Jacob Peak" />
             </div>
         </div>
-    </body>
+
+        <div class="row sample-images">
+            <h2>Recent Photos</h2>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2620.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2620.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2621.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2621.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2622.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2622.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2623.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2623.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2624.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2624.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2626.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2626.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2627.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2627.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2628.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2628.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2630.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2630.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2631.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2631.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2632.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2632.jpg"></a>
+            </div>
+            <div class="col-lg-3 col-md-4 col-sm-6 col-xs-12">
+                <a class="sample-image-link" href="/photos/IMG_2633.jpg" data-lightbox="samples"><img class="sample-image" src="/photos/IMG_2633.jpg"></a>
+            </div>
+        </div>
+
+        <div class="row follow">
+            <h2>Follow Me</h2>
+            <div class="col-xs-12">
+                <a href="https://www.instagram.com/jacobpeakphotography/" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
+                <a href="https://www.facebook.com/Jacob-Peak-Photography-450059072061881/" target="_blank" title="Facebook"><i class="fab fa-facebook"></i></a>
+                <a href="mailto:jacobpeakphotography@gmail.com" title="Email"><i class="far fa-envelope"></i></a>
+            </div>
+        </div>
+
+        <footer class="footer">
+            <p></p>
+        </footer>
+
+    </div> <!-- /container -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+    <script src="/js/lightbox-plus-jquery.min.js"></script>
+    <script src="/js/scripts.js"></script>
+</body>
 </html>
